9e79ec4c2da34b26dce7c402dc18d6624182b76c
Kelvin Lau <kelvin.lau@instacart.com>
2023-02-16 11:28:32 -0800

    Added more snaps

--- Tests/GoodPackageTests/GoodPackageTests.swift
+++ Tests/GoodPackageTests/GoodPackageTests.swift
@@ -5,9 +5,16 @@ import SnapshotTesting
 import UIKit
 
 final class GoodPackageTests: XCTestCase {
-    func testExample() throws {
+    func testAssetColor() throws {
         let text = Text("Hello World")
             .foregroundColor(Color("brandPrimary"))
+            .fixedSize()
+        assertSnapshot(matching: text, as: .image)
+    }
+
+    func testSystemColor() throws {
+        let text = Text("Hello World")
+            .foregroundColor(.blue)
             .fixedSize()
         assertSnapshot(matching: text, as: .image)
     }
